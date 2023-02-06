38942c31f0061f3d0ba68f8fc95939b1760e5ae0
bhumikaChapter247 <bhoomika.b@chapter247.com>
2023-02-06 19:30:28 +0530

    minor changes

--- server/app.js
+++ server/app.js
@@ -1,8 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
-
 import seeders from "./seeders/users.js";
 import router from "./routes/index.js";
 const app = express();
@@ -10,26 +8,26 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use("/api", router);
-dotenv.config(); 
+dotenv.config();
 //configure mongoose
 mongoose.set('strictQuery', false);
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/ERP",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Connected to MongoDB");
-      seeders();
+    process.env.MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connected to MongoDB");
+            seeders();
+        }
     }
-  }
 );
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on port :",process.env.PORT);
+    console.log("Server is running on port :", process.env.PORT);
 });
-export default  app;
+export default app;
