8777c71c8b427bdac4fba033143c91a54e8c23b7
bhumikaChapter247 <bhoomika.b@chapter247.com>
2023-02-07 12:59:13 +0530

    added seperate uploads

--- ui/src/components/Fileload.jsx
+++ ui/src/components/Fileload.jsx
@@ -37,9 +37,35 @@ const Fileload = () => {
 		}
 		console.log("files", files);
 	};
+
+	const onSubmitfile = async (value) => {
+		console.log("val", value.fileList);
+		try {
+			let formData = new FormData();
+			formData.append("files", value.file);
+			formData.append("userId", userDetails._id);
+			const response = await axios.post(
+				"http://localhost:3002/api/user/singleFileUpload",
+				// {file:JSON.stringify(value.file),userId:"63e1d610514403a2e5ec4fb7"}
+				formData
+			);
+			console.log("res------------------------", response);
+			// if (response?.data) {
+			// 	setUser(response.data);
+			// 	localStorage.setItem("token", response.data.token);
+			// 	navigate("/upload");
+			// } else {
+			// 	alert();
+			// }
+		} catch (error) {
+			// alert("Something went wrong");
+		}
+		console.log("files", files);
+	};
 	return (
 		<>
-			<section className='upload-section'>
+			<section className='upload-section d-flex justify-content-around'>
+			
 				<Upload
 					directory
 					multiple
@@ -48,9 +74,22 @@ const Fileload = () => {
 					onChange={(response) => load(response)}
 					customRequest={onSubmit}
 				>
-					<h3>Drag/drop or click to upload files</h3>
+					
 					<div className='d-flex justify-content-center'>
-						<Button icon={<UploadOutlined />}>Upload Files/Folder</Button>
+						<Button icon={<UploadOutlined />}>Upload Folder</Button>
+					</div>
+				</Upload>
+				<Upload
+					
+					multiple
+					// openFileDialogOnClick
+					onRemove={remove}
+					onChange={(response) => load(response)}
+					customRequest={onSubmitfile}
+				>
+					
+					<div className='d-flex justify-content-center'>
+						<Button icon={<UploadOutlined />}>Upload Files</Button>
 					</div>
 				</Upload>
 			</section>
